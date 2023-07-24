59c0e2555ad5ff66b473900ff6b1ff9094dc7944
Stein <steinvlza@gmail.com>
2023-07-23 20:35:30 -0500

    editing...

--- src/app/components/AppointmentsTable.tsx
+++ src/app/components/AppointmentsTable.tsx
@@ -35,7 +35,7 @@ const AppointmentsTable = ({ }) => {
     fetchAppointments();
   }, []);
 
-  const handleDelete = (id) => {
+  const handleDelete = (id: number) => {
     setDeleteId(id);
     setShowConfirmation(true);
   };
